57d4562837e9292a6825ae5cff2ee215b207acb5
Ekaterina Zakharova <e.zakharova.astafieva@gmail.com>
2023-04-04 19:09:32 +0200

    initial commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -35,13 +35,13 @@ public class BonusServiceTest {
         Assertions.assertEquals(expected, actual);
     }
     @Test
-    void shouldCalculateForNoRegisteredAndUnderLimit() {
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1000;
         boolean registered = false;
-        long expected = 30;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -50,13 +50,13 @@ public class BonusServiceTest {
         Assertions.assertEquals(expected, actual);
     }
     @Test
-    void shouldCalculateForNoRegisteredOverLimit() {
+    void shouldCalculateForUnRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1000;
+        long amount = 1_000_000;
         boolean registered = false;
-        long expected = 30;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
